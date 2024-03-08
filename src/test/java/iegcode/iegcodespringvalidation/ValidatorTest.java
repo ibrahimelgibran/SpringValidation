7f98a85ef983e3849f434580528c0228a1bf36c8
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-03-08 11:07:47 +0700

    validator

---
+++ src/test/java/iegcode/iegcodespringvalidation/ValidatorTest.java
@@ -0,0 +1,35 @@
+package iegcode.iegcodespringvalidation;
+
+import iegcode.iegcodespringvalidation.data.Person;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Set;
+
+@SpringBootTest
+public class ValidatorTest {
+
+    @Autowired
+    private Validator validator;
+
+    @Test
+    void personalNotValid() {
+        var person = new Person("", "");
+
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+        Assertions.assertFalse(constraintViolations.isEmpty());
+        Assertions.assertEquals(2, constraintViolations.size());
+    }
+
+    @Test
+    void personValid() {
+        var person = new Person("1", "Gibran");
+
+        Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+        Assertions.assertTrue(constraintViolations.isEmpty());
+    }
+}
